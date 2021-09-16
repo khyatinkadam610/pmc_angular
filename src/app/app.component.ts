05ecc9bb283e6c46710f6e7a3c4135d762479957
khyatin <khyatinkadam610@gmail.com>
2021-09-16 11:07:29 +0530

    after 3Rd part

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template:`
+  <div>
+  <h1>{{pageTitle}}</h1>
+  <div>Mt First Component</div>
+  </div>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pageTitle = 'Acme Project Management';
+}
